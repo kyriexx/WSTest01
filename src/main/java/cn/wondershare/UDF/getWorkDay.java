9afe0fb52297a8d7c9317d20800b5f57a4d13453
kyrie <kyrie@wondershare.cn>
2021-07-21 17:05:14 +0800

    idea 在master分支上第3次提交

--- src/main/java/cn/wondershare/UDF/getWorkDay.java
+++ src/main/java/cn/wondershare/UDF/getWorkDay.java
@@ -57,6 +57,7 @@ public class getWorkDay {
 
     public static void getDaysByYear(int year) {
         Calendar c = Calendar.getInstance();
+        System.out.println("测试master分支");
         //List dates = new ArrayList();
         for (int i = 0; i < 12; i++) {
             c.set(year, i, 1);
